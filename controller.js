1b75f58d49dadc4d856fcb1c48f0a419377c474b
aristotelessiregar86 <aristotelessiregar89@gmial.com>
2021-03-13 09:31:39 +0700

    Menambahkan File Controller

--- controller.js
+++ controller.js
@@ -1,1 +1,7 @@
-// const response = 
+'use strict';
+const response = require('./res');
+const connection = require('./koneksi');
+
+exports.index = function (req, res) {
+        response.ok('Aris Aplikasi Berjalan....');
+};
